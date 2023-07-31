dcb7ae3c78a2c036c07b38f42bf51e2e5647d6da
Documenter.jl <documenter@juliadocs.github.io>
2023-07-31 15:21:24 +0000

    build based on 81611ff

--- dev/guide.jl
+++ dev/guide.jl
@@ -1,5 +1,5 @@
 # # Getting Started
-# SIMDiff can create nonlinear prgogramming models and allows solving the created models using NLP solvers (in particular, those that are interfaced with `NLPModels`, such as [NLPModelsIpopt](https://github.com/JuliaSmoothOptimizers/NLPModelsIpopt.jl). We now use `SIMDiff` to model the following nonlinear program:
+# ExaModels can create nonlinear prgogramming models and allows solving the created models using NLP solvers (in particular, those that are interfaced with `NLPModels`, such as [NLPModelsIpopt](https://github.com/JuliaSmoothOptimizers/NLPModelsIpopt.jl). We now use `ExaModels` to model the following nonlinear program:
 # ```math
 # \begin{aligned}
 # \min_{\{x_i\}_{i=0}^N} &\sum_{i=2}^N  100(x_{i-1}^2-x_i)^2+(x_{i-1}-1)^2\\
@@ -7,32 +7,32 @@
 # \end{aligned}
 # ```
 # We model the problem with:
-using SIMDiff
+using ExaModels
 
 # We set
 N = 10000
 
-# First, we create a `SIMDiff.Core`.
-c = SIMDiff.Core()
+# First, we create a `ExaModels.Core`.
+c = ExaModels.Core()
 
 # The variables can be created as follows:
 
-x = SIMDiff.variable(
+x = ExaModels.variable(
     c, N;
     start = (mod(i,2)==1 ? -1.2 : 1. for i=1:N)
 )
 
 # The objective can be set as follows:
-SIMDiff.objective(c, 100*(x[i-1]^2-x[i])^2+(x[i-1]-1)^2 for i in 2:N)
+ExaModels.objective(c, 100*(x[i-1]^2-x[i])^2+(x[i-1]-1)^2 for i in 2:N)
 
 # The constraints can be set as follows:
-SIMDiff.constraint(
+ExaModels.constraint(
     c,
     3x[i+1]^3+2*x[i+2]-5+sin(x[i+1]-x[i+2])sin(x[i+1]+x[i+2])+4x[i+1]-x[i]exp(x[i]-x[i+1])-3
     for i in 1:N-2)
 
 # Finally, we create an NLPModel.
-m = SIMDiff.Model(c)
+m = ExaModels.Model(c)
 
 # To solve the problem with `Ipopt`,
 using NLPModelsIpopt
