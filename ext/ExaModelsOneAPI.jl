ddaeec24646f134d48e208cc9bedc3180044653e
Sungho Shin <sshin@anl.gov>
2023-08-26 22:26:22 -0500

    type privacy removed (#19)

--- ext/ExaModelsOneAPI.jl
+++ ext/ExaModelsOneAPI.jl
@@ -6,14 +6,14 @@ ExaModels.ExaCore(backend::oneAPI.oneAPIBackend) = ExaModels.ExaCore(Float64, ba
 ExaModels.ExaCore(T, backend::oneAPI.oneAPIBackend) =
     ExaModels.ExaCore(x0 = oneAPI.zeros(T, 0), backend = backend)
 
-function ExaModels.myappend!(a::A, b::Base.Generator, lb) where {A<:oneAPI.oneVector}
+function ExaModels.append!(a::A, b::Base.Generator, lb) where {A<:oneAPI.oneVector}
     la = length(a)
     a = similar(a, la + lb)
     map!(b.f, view(a, (la+1):(la+lb)), b.iter)
     return a
 end
 
-function ExaModels.myappend!(a::A, b::A, lb) where {A<:oneAPI.oneVector}
+function ExaModels.append!(a::A, b::A, lb) where {A<:oneAPI.oneVector}
     la = length(a)
     a = similar(a, la + lb)
     copyto!(view(a, (la+1):(la+lb)), b)
@@ -21,7 +21,7 @@ function ExaModels.myappend!(a::A, b::A, lb) where {A<:oneAPI.oneVector}
 end
 
 
-function ExaModels.myappend!(a::A, b::Number, lb) where {A<:oneAPI.oneVector}
+function ExaModels.append!(a::A, b::Number, lb) where {A<:oneAPI.oneVector}
     la = length(a)
     a = similar(a, la + lb)
     fill!(view(a, (la+1):(la+lb)), b)
@@ -30,19 +30,16 @@ end
 
 ExaModels.convert_array(v, backend::oneAPI.oneAPIBackend) = oneAPI.oneArray(v)
 
-# Below are type piracy
-function Base.sum(a::A) where {A<:oneAPI.oneVector{Float64}}
-    b = zeros(length(a))
-    copyto!(b, a)
-    return sum(b)
+function ExaModels.sum(a::A) where {A<:oneAPI.oneVector{Float64}}
+    return Float64(mapreduce(Float32, +, a))
 end
-function Base.findall(f::F, bitarray::A) where {F<:Function,A<:oneAPI.oneVector}
+function ExaModels.findall(f::F, bitarray::A) where {F<:Function,A<:oneAPI.oneVector}
     a = Array(bitarray)
     b = findall(f, a)
     c = similar(bitarray, eltype(b), length(b))
     return copyto!(c, b)
 end
-Base.findall(bitarray::A) where {A<:oneAPI.oneVector} = Base.findall(identity, bitarray)
-Base.sort!(array::A; lt = isless) where {A<:oneAPI.oneVector} =
+ExaModels.findall(bitarray::A) where {A<:oneAPI.oneVector} = ExaModels.findall(identity, bitarray)
+ExaModels.sort!(array::A; lt = isless) where {A<:oneAPI.oneVector} =
     copyto!(array, sort!(Array(array)))
 end
--- src/templates.jl
+++ src/templates.jl
@@ -1,1 +1,7 @@
 convert_array(v, ::Nothing) = v
+
+# to avoid type privacy
+sum(a) = Base.sum(a)
+findall(f, bitarray) = Base.findall(f, bitarray)
+findall(bitarray) = Base.findall(bitarray)
+sort!(array; kwargs...) = Base.sort!(array; kwargs...)
