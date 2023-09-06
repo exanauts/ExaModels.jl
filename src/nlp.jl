ab5ba18167ca513420e32148439c25d55aea4ce6
Sungho Shin <sshin@anl.gov>
2023-09-06 17:46:49 -0500

    kwargs

--- src/nlp.jl
+++ src/nlp.jl
@@ -213,7 +213,7 @@ julia> result = ipopt(m; print_level=0)    # solve the problem
 
 ```
 """
-function ExaModel(c::C) where {C<:ExaCore}
+function ExaModel(c::C; kwargs...) where {C<:ExaCore}
     return ExaModel(
         c.obj,
         c.con,
