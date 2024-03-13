2d88c5a2e272fed6f12505c900ab05d30928b648
Sungho Shin <sshin@anl.gov>
2024-03-12 23:04:42 -0500

    Double index constraints

--- ext/ExaModelsCUDA.jl
+++ ext/ExaModelsCUDA.jl
@@ -4,6 +4,9 @@ import ExaModels: ExaModels, NLPModels
 import CUDA: CUDA, CUDABackend, CuArray
 
 ExaModels.ExaCore(backend::CUDABackend) = ExaModels.ExaCore(Float64, backend)
+ExaModels.convert_array(v::Base.Iterators.ProductIterator, backend::CUDABackend) =
+    Base.product((ExaModels.convert_array(i, backend) for i in v.iterators)...)
+ExaModels.convert_array(v::UnitRange, backend::CUDABackend) = v
 ExaModels.convert_array(v, backend::CUDABackend) = CuArray(v)
 
 end
