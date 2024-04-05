0f4e539da39952d381ab55c66c0290dfb98616fd
Sungho Shin <sshin@anl.gov>
2024-04-05 14:57:24 -0500

    rollback changes in ext packages

--- ext/ExaModelsAMDGPU.jl
+++ ext/ExaModelsAMDGPU.jl
@@ -4,4 +4,16 @@ import ExaModels, AMDGPU
 
 ExaModels.convert_array(v, backend::AMDGPU.ROCBackend) = AMDGPU.ROCArray(v)
 
+ExaModels.sort!(array::A; lt = isless) where {A<:AMDGPU.ROCVector} =
+    copyto!(array, sort!(Array(array); lt = lt))
+
+# Below are type piracy
+function Base.findall(f::F, bitarray::A) where {F<:Function,A<:AMDGPU.ROCVector}
+    a = Array(bitarray)
+    b = findall(f, a)
+    c = similar(bitarray, eltype(b), length(b))
+
+    return copyto!(c, b)
+end
+Base.findall(bitarray::A) where {A<:AMDGPU.ROCVector} = Base.findall(identity, bitarray)
 end
--- ext/ExaModelsOneAPI.jl
+++ ext/ExaModelsOneAPI.jl
@@ -50,4 +50,14 @@ ExaModels.convert_array(v, backend::oneAPI.oneAPIBackend) = oneAPI.oneArray(v)
 
 ExaModels.sort!(array::A; lt = isless) where {A<:oneAPI.oneArray} =
     copyto!(array, sort!(Array(array); lt = lt))
+
+# below is type piracy
+function Base.findall(f::F, bitarray::A) where {F<:Function,A<:oneAPI.oneArray}
+    a = Array(bitarray)
+    b = findall(f, a)
+    c = similar(bitarray, eltype(b), length(b))
+    return copyto!(c, b)
+end
+Base.findall(bitarray::A) where {A<:oneAPI.oneArray} = Base.findall(identity, bitarray)
+
 end # module
