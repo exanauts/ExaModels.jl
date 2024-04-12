5a26eb48297452ba46dc8497d7a90aeb59d009b0
Sungho Shin <sshin@anl.gov>
2024-04-12 10:40:40 -0500

    Docs update

--- docs/src/jump.jl
+++ docs/src/jump.jl
@@ -22,7 +22,7 @@ em = ExaModel(jm; backend = CUDABackend())
 # Here, note that only scalar objective/constraints created via `@constraint` and `@objective` API are supported. Older syntax like `@NLconstraint` and `@NLobjective` are not supported.
 # We can solve the model using any of the solvers supported by ExaModels. For example, we can use MadNLP:
 
-using MadNLP, MadNLPGPU
+using MadNLPGPU
 
 result = madnlp(em)
 
@@ -31,7 +31,7 @@ result = madnlp(em)
 # Alternatively, one can use the `Optimizer` interface provided by `ExaModels`. This feature can be used as follows.
 
 using ExaModels, JuMP, CUDA
-using MadNLP, MadNLPGPU
+using MadNLPGPU
 
 set_optimizer(jm, () -> ExaModels.MadNLPOptimizer(CUDABackend()))
 optimize!(jm)
