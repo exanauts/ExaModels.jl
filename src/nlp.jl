37604aff7fa4656ba2422121834169073a5b93af
Sungho Shin <sshin@anl.gov>
2024-11-05 21:58:24 +0000

    docstring change

--- src/nlp.jl
+++ src/nlp.jl
@@ -112,7 +112,7 @@ julia> c = ExaCore(Float32; backend = CUDABackend())
 An ExaCore
 
   Float type: ...................... Float32
-  Array type: ...................... CUDA.CuArray{Float32, 1, CUDA.DeviceBuffer}
+  Array type: ...................... CUDA.CuArray{Float32, 1, CUDA.DeviceMemory}
   Backend: ......................... CUDA.CUDAKernels.CUDABackend
 
   number of objective patterns: .... 0
