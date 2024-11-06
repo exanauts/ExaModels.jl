99d932d7c496d32beeaca2a6bae3e0f84b5fb7ba
François Pacaud <frapac@users.noreply.github.com>
2024-11-06 03:53:07 +0100

    Pass option  to ExaModel when instantiating from JuMP (#97)
    Co-authored-by: Sungho Shin <sshin@anl.gov>

--- ext/ExaModelsJuMP.jl
+++ ext/ExaModelsJuMP.jl
@@ -3,8 +3,8 @@ module ExaModelsJuMP
 import ExaModels
 import JuMP
 
-function ExaModels.ExaModel(jm::JuMP.GenericModel{T}; backend = nothing) where {T}
-    return ExaModels.ExaModel(jm.moi_backend.model_cache; backend = backend)
+function ExaModels.ExaModel(jm::JuMP.GenericModel{T}; options...) where {T}
+    return ExaModels.ExaModel(jm.moi_backend.model_cache; options...)
 end
 
 end # module ExaModelsJuMP
