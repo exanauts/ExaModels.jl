b74e2d0a39ae7d9164ed96553fc0f859505c8180
Sungho Shin <sshin@anl.gov>
2025-01-12 14:53:23 -0500

    see if it works on ci

--- ext/ExaModelsAMDGPU.jl
+++ ext/ExaModelsAMDGPU.jl
@@ -3,12 +3,12 @@ module ExaModelsAMDGPU
 import ExaModels, AMDGPU
 
 # Below are type piracy
-function Base.findall(f::F, bitarray::A) where {F<:Function,A<:AMDGPU.ROCVector}
-    a = Array(bitarray)
-    b = findall(f, a)
-    c = similar(bitarray, eltype(b), length(b))
+# function Base.findall(f::F, bitarray::A) where {F<:Function,A<:AMDGPU.ROCVector}
+#     a = Array(bitarray)
+#     b = findall(f, a)
+#     c = similar(bitarray, eltype(b), length(b))
 
-    return copyto!(c, b)
-end
-Base.findall(bitarray::A) where {A<:AMDGPU.ROCVector} = Base.findall(identity, bitarray)
+#     return copyto!(c, b)
+# end
+# Base.findall(bitarray::A) where {A<:AMDGPU.ROCVector} = Base.findall(identity, bitarray)
 end
