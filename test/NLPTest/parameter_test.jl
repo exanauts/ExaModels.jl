af74a3dbedbfad969755b56de857e6cb4583dc08
Klamkin, Michael <mklamkin3@gatech.edu>
2025-06-25 15:54:41 -0400

    refactor test

--- test/NLPTest/parameter_test.jl
+++ test/NLPTest/parameter_test.jl
@@ -17,87 +17,127 @@ function luksan_vlcek_con2_param(x, θ, i, j)
            x[i, j]exp(x[i, j] - x[i+1, j]) - θ[7]
 end
 
-function exa_luksan_vlcek_parametric(backend, N; M = 1)
+function exa_luksan_vlcek_parametric(backend, N; M=1, use_parameters=true, param_values=nothing)
     c = ExaCore(backend = backend)
     x = variable(c, N, M; start = [luksan_vlcek_x0(i) for i = 1:N, j = 1:M])
     
-    param_values = [100.0, 1.0, 3.0, 2.0, 5.0, 4.0, 3.0]
-    θ = parameter(c, param_values)
-    
-    s = constraint(c, luksan_vlcek_con1_param(x, θ, i, j) for i = 1:(N-2), j = 1:M)
-    constraint!(c, s, (i, j) => luksan_vlcek_con2_param(x, θ, i, j) for i = 1:(N-2), j = 1:M)
-    objective(c, luksan_vlcek_obj_param(x, θ, i, j) for i = 2:N, j = 1:M)
+    if use_parameters
+        θ = parameter(c, 1:7)
+        if !isnothing(param_values)
+            c.θ .= param_values
+        else
+            c.θ .= [100.0, 1.0, 3.0, 2.0, 5.0, 4.0, 3.0]
+        end
 
-    return ExaModel(c; prod = true), (x,), (s,), θ
+        s = constraint(c, luksan_vlcek_con1_param(x, θ, i, j) for i = 1:(N-2), j = 1:M)
+        constraint!(c, s, (i, j) => luksan_vlcek_con2_param(x, θ, i, j) for i = 1:(N-2), j = 1:M)
+        objective(c, luksan_vlcek_obj_param(x, θ, i, j) for i = 2:N, j = 1:M)
+    else
+        if isnothing(param_values)
+            param_values = [100.0, 1.0, 3.0, 2.0, 5.0, 4.0, 3.0]
+        end
+
+        obj_func = (x, i, j) -> param_values[1] * (x[i-1, j]^2 - x[i, j])^2 + (x[i-1, j] - param_values[2])^2
+        con1_func = (x, i, j) -> param_values[3] * x[i+1, j]^3 + param_values[4] * x[i+2, j] - param_values[5]
+        con2_func = (x, i, j) -> sin(x[i+1, j] - x[i+2, j])sin(x[i+1, j] + x[i+2, j]) + param_values[6] * x[i+1, j] - x[i, j]exp(x[i, j] - x[i+1, j]) - param_values[7]
+        
+        s = constraint(c, con1_func(x, i, j) for i = 1:(N-2), j = 1:M)
+        constraint!(c, s, (i, j) => con2_func(x, i, j) for i = 1:(N-2), j = 1:M)
+        objective(c, obj_func(x, i, j) for i = 2:N, j = 1:M)
+    end
+    
+    return ExaModel(c; prod = true), c, (x,), (s,)
+end
+
+function test_function_evaluations(model1, core1, model2, core2)
+    x_test = randn(core1.nvar)
+
+    obj1 = NLPModels.obj(model1, x_test)
+    obj2 = NLPModels.obj(model2, x_test)
+    @test obj1 ≈ obj2 atol=1e-12
+
+    if core1.ncon > 0
+        con1 = NLPModels.cons(model1, x_test)
+        con2 = NLPModels.cons(model2, x_test)
+        @test con1 ≈ con2 atol=1e-12
+    end
+
+    grad1 = NLPModels.grad(model1, x_test)
+    grad2 = NLPModels.grad(model2, x_test)
+    @test grad1 ≈ grad2 atol=1e-12
+    
+    u = randn(core1.nvar)
+    v = randn(core1.ncon)
+    jprod_param = NLPModels.jprod(model2, x_test, u)
+    jprod_orig = NLPModels.jprod(model1, x_test, u)
+    @test jprod_param ≈ jprod_orig atol=1e-12
+
+    jtprod_param = NLPModels.jtprod(model2, x_test, v)
+    jtprod_orig = NLPModels.jtprod(model1, x_test, v)
+    @test jtprod_param ≈ jtprod_orig atol=1e-12
+
+    y_test = randn(core1.ncon)
+    hprod_param = NLPModels.hprod(model2, x_test, y_test, u)
+    hprod_orig = NLPModels.hprod(model1, x_test, y_test, u)
+    @test hprod_param ≈ hprod_orig atol=1e-12
 end
 
 function test_parametric_vs_nonparametric(backend)
-    @testset "Parametric vs Non-parametric Luksan Model" begin
-        N = 5
-        M = 1
+    @testset "Parametric" begin
+        @testset "Metadata" begin
+            m_param, c_param, _, _ = exa_luksan_vlcek_parametric(
+                backend, 4, M=3, use_parameters=true)
+            m_nonparam, c_nonparam, _, _ = exa_luksan_vlcek_parametric(
+                backend, 4, M=3, use_parameters=false)
+            @test c_param.nvar == c_nonparam.nvar
+            @test c_param.ncon == c_nonparam.ncon
+            @test ExaModels.depth(c_param.obj) == ExaModels.depth(c_nonparam.obj) 
+            @test ExaModels.depth(c_param.con) == ExaModels.depth(c_nonparam.con)
+            @test c_param.npar == 7
+            @test c_nonparam.npar == 0
+        end
 
-        m_param, vars_param, cons_param, θ = exa_luksan_vlcek_parametric(backend, N; M = M)
-        m_orig, vars_orig, cons_orig = _exa_luksan_vlcek_model(backend, N; M = M)
-
-        @testset "Model Structure" begin
-            @test m_param.meta.nvar == m_orig.meta.nvar
-            @test m_param.meta.ncon == m_orig.meta.ncon
-            @test m_param.meta.nnzj == m_orig.meta.nnzj
-            @test m_param.meta.nnzh == m_orig.meta.nnzh
-            @test m_param.meta.x0 ≈ m_orig.meta.x0
-            @test m_param.meta.lvar ≈ m_orig.meta.lvar
-            @test m_param.meta.uvar ≈ m_orig.meta.uvar
-            @test m_param.meta.y0 ≈ m_orig.meta.y0
-            @test m_param.meta.lcon ≈ m_orig.meta.lcon
-            @test m_param.meta.ucon ≈ m_orig.meta.ucon
+        @testset "Default" begin
+            m_param, c_param, _, _ = exa_luksan_vlcek_parametric(
+                backend, 3, M=2, use_parameters=true)
+            m_nonparam, c_nonparam, _, _ = exa_luksan_vlcek_parametric(
+                backend, 3, M=2, use_parameters=false)
+            test_function_evaluations(m_param, c_param, m_nonparam, c_nonparam)
+        end
+        @testset "Modified Parameters objective" begin
+            modified_params1 = [200.0, 2.0, 3.0, 2.0, 5.0, 4.0, 3.0]
+            m_param, c_param, _, _ = exa_luksan_vlcek_parametric(
+                backend, 3, M=2, use_parameters=true, param_values=modified_params1)
+            m_nonparam, c_nonparam, _, _ = exa_luksan_vlcek_parametric(
+                backend, 3, M=2, use_parameters=false, param_values=modified_params1)
+            test_function_evaluations(m_param, c_param, m_nonparam, c_nonparam)
         end
         
-        x_test = copy(m_orig.meta.x0)
-        y_test = randn(m_orig.meta.ncon)
-        @testset "Function Evaluations" begin
-            obj_param = NLPModels.obj(m_param, x_test)
-            obj_orig = NLPModels.obj(m_orig, x_test)
-            @test obj_param ≈ obj_orig atol=1e-12
-            
-            cons_param = NLPModels.cons(m_param, x_test)
-            cons_orig = NLPModels.cons(m_orig, x_test)
-            @test cons_param ≈ cons_orig atol=1e-12
-            
-            grad_param = NLPModels.grad(m_param, x_test)
-            grad_orig = NLPModels.grad(m_orig, x_test)
-            @test grad_param ≈ grad_orig atol=1e-12
-            
-            if m_orig.meta.ncon > 0
-                u = randn(m_orig.meta.nvar)
-                v = randn(m_orig.meta.ncon)
-                
-                jprod_param = NLPModels.jprod(m_param, x_test, u)
-                jprod_orig = NLPModels.jprod(m_orig, x_test, u)
-                @test jprod_param ≈ jprod_orig atol=1e-12
-                
-                jtprod_param = NLPModels.jtprod(m_param, x_test, v)
-                jtprod_orig = NLPModels.jtprod(m_orig, x_test, v)
-                @test jtprod_param ≈ jtprod_orig atol=1e-12
-                
-                hprod_param = NLPModels.hprod(m_param, x_test, y_test, u)
-                hprod_orig = NLPModels.hprod(m_orig, x_test, y_test, u)
-                @test hprod_param ≈ hprod_orig atol=1e-12
-            end
+        @testset "Modified Parameters constraints" begin
+            modified_params2 = [100.0, 1.0, 6.0, 4.0, 10.0, -8.0, 6.0]
+            m_param, c_param, _, _ = exa_luksan_vlcek_parametric(
+                backend, 3, M=2, use_parameters=true, param_values=modified_params2)
+            m_nonparam, c_nonparam, _, _ = exa_luksan_vlcek_parametric(
+                backend, 3, M=2, use_parameters=false, param_values=modified_params2)
+            test_function_evaluations(m_param, c_param, m_nonparam, c_nonparam)
+        end
+        @testset "Modified Parameters all" begin
+            modified_params3 = [150.0, 0.5, 2.5, 1.5, 7.5, 3.5, 4.5]
+            m_param, c_param, _, _ = exa_luksan_vlcek_parametric(
+                backend, 3, M=2, use_parameters=true, param_values=modified_params3)
+            m_nonparam, c_nonparam, _, _ = exa_luksan_vlcek_parametric(
+                backend, 3, M=2, use_parameters=false, param_values=modified_params3)
+            test_function_evaluations(m_param, c_param, m_nonparam, c_nonparam)
         end
         
-        @testset "Parameter Modification" begin
-            original_param_values = copy(m_param.θ)
-            
-            m_param.θ[1] = 200.0
-            
-            obj_modified = NLPModels.obj(m_param, x_test)
-            obj_orig = NLPModels.obj(m_orig, x_test)
-            
-            @test obj_modified ≠ obj_orig
-            
-            m_param.θ[1] = original_param_values[1]
-            obj_restored = NLPModels.obj(m_param, x_test)
-            @test obj_restored ≈ obj_orig atol=1e-12
+        @testset "Modify after build" begin
+            m_param, c_param, _, _ = exa_luksan_vlcek_parametric(
+                backend, 3, M=2, use_parameters=true)
+            new_params = [75.0, 1.5, 4.0, 3.0, 6.0, 5.0, 2.0]
+            c_param.θ .= new_params
+            m_nonparam, c_nonparam, _, _ = exa_luksan_vlcek_parametric(
+                backend, 3, M=2, use_parameters=false, param_values=new_params)
+            test_function_evaluations(m_param, c_param, m_nonparam, c_nonparam)
         end
     end
 end 
