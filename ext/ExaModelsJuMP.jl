9f43dbc047ff5edf1935ab2d94a8a6a10988d2dc
Klamkin, Michael <mklamkin3@gatech.edu>
2025-07-02 17:46:32 -0400

    [skip ci] remove fixme

--- ext/ExaModelsJuMP.jl
+++ ext/ExaModelsJuMP.jl
@@ -4,7 +4,6 @@ import ExaModels
 import JuMP
 
 function ExaModels.ExaModel(jm::JuMP.GenericModel{T}; options...) where {T}
-    # FIXME: what is user passes `T` under `options`?
     return ExaModels.ExaModel(JuMP.backend(jm); T=T, options...)
 end
 
