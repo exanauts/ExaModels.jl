b8d56d2775257eb2daa1cfb760783cf232d82cf6
Sungho Shin <sushin@mit.edu>
2025-07-27 21:49:01 -0400

    be more forgiving for non-bittype arrays

--- src/simdfunction.jl
+++ src/simdfunction.jl
@@ -37,10 +37,6 @@ Returns a `SIMDFunction` using the `gen`.
 """
 function SIMDFunction(gen::Base.Generator, o0 = 0, o1 = 0, o2 = 0)
 
-    if !isbitstype(eltype(gen.iter))
-        error("The generator must be iterable with a bitstype element.")
-    end
-    
     f = gen.f(Par(eltype(gen.iter)))
 
     _simdfunction(f, o0, o1, o2)
