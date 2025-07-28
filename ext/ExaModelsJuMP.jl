3622d84ce8f1d974539001a042c5095f501ab209
Michael Klamkin <klamike@users.noreply.github.com>
2025-07-28 16:43:20 -0400

    Update MOI extension (#155)
    * safe moi
    
    * add maps + refactor
    
    * maps optional
    
    * refactor
    
    * update
    
    * check functions/obj too
    
    * make MOI index types
    
    * to_exacore
    
    * init parameter support
    
    * don't try to detect T
    
    * short-circuit if no cis
    
    * proper param index
    
    * cleanup
    
    * cleanup cont.
    
    * typo
    
    * [skip ci] remove fixme
    
    * typo
    
    * [skip ci] remove fixme
    
    * tests
    
    * Update test/JuMPTest/JuMPTest.jl

--- ext/ExaModelsJuMP.jl
+++ ext/ExaModelsJuMP.jl
@@ -4,7 +4,7 @@ import ExaModels
 import JuMP
 
 function ExaModels.ExaModel(jm::JuMP.GenericModel{T}; options...) where {T}
-    return ExaModels.ExaModel(jm.moi_backend.model_cache; options...)
+    return ExaModels.ExaModel(JuMP.backend(jm); T=T, options...)
 end
 
 end # module ExaModelsJuMP
