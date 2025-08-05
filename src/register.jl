3b3f4532cbdb50609faa7fb7e530d3b66e060ebf
Sungho Shin <sushin@mit.edu>
2025-08-05 10:44:46 -0400

    Docstring fixed for register

--- src/register.jl
+++ src/register.jl
@@ -59,22 +59,22 @@ Register a bivariate function `f` to `ExaModels`, so that it can be used within 
 ```jldoctest
 julia> using ExaModels
 
-julia> relu23(x) = (x > 0 || y > 0) ? (x + y)^3 : zero(x)
+julia> relu23(x,y) = (x > 0 || y > 0) ? (x + y)^3 : zero(x)
 relu23 (generic function with 1 method)
 
-julia> drelu231(x) = (x > 0 || y > 0) ? 3 * (x + y)^2 : zero(x)
+julia> drelu231(x,y) = (x > 0 || y > 0) ? 3 * (x + y)^2 : zero(x)
 drelu231 (generic function with 1 method)
 
-julia> drelu232(x) = (x > 0 || y > 0) ? 3 * (x + y)^2  : zero(x)
+julia> drelu232(x,y) = (x > 0 || y > 0) ? 3 * (x + y)^2  : zero(x)
 drelu232 (generic function with 1 method)
 
-julia> ddrelu2311(x) = (x > 0 || y > 0) ? 6 * (x + y) : zero(x)
+julia> ddrelu2311(x,y) = (x > 0 || y > 0) ? 6 * (x + y) : zero(x)
 ddrelu2311 (generic function with 1 method)
 
-julia> ddrelu2312(x) = (x > 0 || y > 0) ? 6 * (x + y) : zero(x)
+julia> ddrelu2312(x,y) = (x > 0 || y > 0) ? 6 * (x + y) : zero(x)
 ddrelu2312 (generic function with 1 method)
 
-julia> ddrelu2322(x) = (x > 0 || y > 0) ? 6 * (x + y) : zero(x)
+julia> ddrelu2322(x,y) = (x > 0 || y > 0) ? 6 * (x + y) : zero(x)
 ddrelu2322 (generic function with 1 method)
 
 julia> @register_bivariate(relu23, drelu231, drelu232, ddrelu2311, ddrelu2312, ddrelu2322)
