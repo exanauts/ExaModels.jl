a81bc37f0cba866d26d18fbbe9e1303154b729aa
Sungho Shin <sushin@mit.edu>
2026-02-28 23:16:45 -0500

    naming resolved

--- src/gradient.jl
+++ src/gradient.jl
@@ -68,7 +68,7 @@ Performs dsparse gradient evaluation via the reverse pass on the computation (su
     o1,
     cnt,
     adj,
-) where {D<:Union{AdjointNull,ParIndexed,Real}}
+) where {D<:Union{AdjointNull,DataIndexed,Real}}
     return cnt
 end
 @inline function grpass(d::D, comp, y, o1, cnt, adj) where {D<:AdjointNode1}
@@ -104,7 +104,7 @@ end
     o1,
     cnt::Tuple{<:Tuple,<:Tuple},
     adj,
-) where {D<:Union{AdjointNull,ParIndexed,Real}}
+) where {D<:Union{AdjointNull,DataIndexed,Real}}
     return cnt
 end
 @inline function grpass(
